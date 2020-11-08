69e5bd56a98b8c33ac76dca18d9bda6ee0d9bebd
liyb <1055025755@qq.com>
2020-11-08 20:49:04 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home1</router-link> |
-      <router-link to="/about">About1</router-link>
+      <router-link to="/">Home2</router-link> |
+      <router-link to="/about">About2</router-link>
     </div>
     <router-view />
   </div>
